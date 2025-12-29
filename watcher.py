131c1fd2fba7907d2dcb0fca8fc98db46d7bd68c
Evan Schwalb <evanschwalb@gmail.com>
2025-12-28 16:37:59 -0800

    Added pywin32 to look for windows

--- watcher.py
+++ watcher.py
@@ -1,3 +1,9 @@
-import watchdog 
 import win32gui 
+import asyncio
+class checkForFiles: 
+    while(True):
+        whatisit = win32gui.GetForegroundWindow()
+        print(win32gui.GetWindowText(whatisit))
 
+if __name__ == "__main__": 
+    pass
