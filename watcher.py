aca9fe6ba3f4daf4e2ffa7f0164255dddc344cdc
Evan Schwalb <evanschwalb@gmail.com>
2025-12-30 11:58:36 -0800

    added new file and getting closer to getting actual text

--- watcher.py
+++ watcher.py
@@ -1,13 +1,20 @@
 import win32gui 
 import asyncio
-import pywinauto 
+from pywinauto import Application
 
 class checkForFiles: 
     def isUserinAI(): 
         while(True):
             whatisit = win32gui.GetForegroundWindow()
             if "Google Gemini" in win32gui.GetWindowText(whatisit): 
-                    print(win32gui.Get)
+                    geminiTitle = win32gui.GetWindowText(whatisit)
+                    app = Application(backend="uia").connect(title_re=".*Gemini.*")
+                    dlg = app.window(title_re=".*Gemini.*")
+                    for ctrl in dlg.descendants():
+                        if "chat-app" in ctrl.class_name():
+                            pass    
+                            
+                    
                 
 
 if __name__ == "__main__": 
