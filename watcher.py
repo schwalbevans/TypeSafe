927cf4da5c118f797c3460d892c8b8aad172ede9
Evan Schwalb <evanschwalb@gmail.com>
2025-12-30 21:04:20 -0800

    too rtired

--- watcher.py
+++ watcher.py
@@ -10,13 +10,14 @@ class checkForFiles:
                     geminiTitle = win32gui.GetWindowText(whatisit)
                     app = Application(backend="uia").connect(title_re=".*Gemini.*")
                     dlg = app.window(title_re=".*Gemini.*")
-                    for ctrl in dlg.descendants():
-                        if "chat-app" in ctrl.class_name():
-                            pass    
-                            
-                    
-                
-
+                    for ctrl in dlg.descendants():   
+                        if "ql-editor textarea" in ctrl.class_name():
+                            print(f"Type: {ctrl.class_name()} | Title: '{ctrl.window_text()}'")
+                         #   try:
+                         #       print(f"User Input: {ctrl.get_value()}")
+                         #   except Exception:
+                         #       print(f"User Input: {ctrl.window_text()}")
+                    exit()
 if __name__ == "__main__": 
     checkForFiles.isUserinAI() 
     
